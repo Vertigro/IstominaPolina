f8c983f0bd4b9165037f081052bd76fb729d813c
Vertigro <114555035+Vertigro@users.noreply.github.com>
2022-09-28 11:41:01 +0700

    Add files via upload

---
+++ Истомина.py
@@ -0,0 +1,8 @@
+x = int(input("x"))
+print(x)
+y = int(input("y"))
+print(y)
+for i in range (1,y):
+i = x*x
+#n = x**y
+print (i)
---
+++ Истомина14.09.py
@@ -0,0 +1,8 @@
+N10 = (int(0))
+Np = (int(input("введите исходное число"  f"N{p}="))
+K = (int(1))
+#Longint = (int(input())
+p = (int(input("Введите основание системы" "p = "))
+while  not Np == 0:
+    N10 = N10 + ()
+     
