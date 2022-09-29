747776e1ecd03e45811aafbea8e8ed69585ff607
Vertigro <114555035+Vertigro@users.noreply.github.com>
2022-09-29 11:42:24 +0700

    Delete Истомина14.09.py

--- Истомина14.09.py
+++
@@ -1,8 +0,0 @@
-N10 = (int(0))
-Np = (int(input("введите исходное число"  f"N{p}="))
-K = (int(1))
-#Longint = (int(input())
-p = (int(input("Введите основание системы" "p = "))
-while  not Np == 0:
-    N10 = N10 + ()
-     
