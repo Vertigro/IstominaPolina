08561b877ff3aa60c9bd30f8e383eed98cb8c5d8
Vertigro <114555035+Vertigro@users.noreply.github.com>
2022-12-14 12:00:47 +0700

    Add files via upload

---
+++ Gthtdjhfxbdfybt afvbkbb.py
@@ -0,0 +1,27 @@
+def p(f):
+    print(f[::-1])
+
+def b(c):
+    return(f[::-1])
+    return (m)
+
+f = input('Введите свою фамилию')
+m = input ('Введите свой пол')
+p(f)
+
+c = b(f)
+
+
+print(c)
+print (m)
+for i in f: print(i, end=' ') 
+a=list(f)
+
+g = ['а', 'у', 'о', 'ы', 'э', 'я', 'ю', 'ё', 'и', 'е',]
+for i in g:
+    if (g[i])== a:
+            print (g[i])
+
+
+ 
+    
