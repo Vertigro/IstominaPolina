6e5db91514819151b78f1616780535d2d0448a05
Vertigro <114555035+Vertigro@users.noreply.github.com>
2023-01-25 11:49:40 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -69,5 +69,9 @@ def f14():
         if f % 14 == 0:
             print(x,f//14)
             break 
-
+def f15():
+    for a in range(1,1000):
+    if all (((x%2==0) <= (x%3!=0)) or (x+a>=100)for x in range(1,10000)):
+        print(a)
+        break
              
