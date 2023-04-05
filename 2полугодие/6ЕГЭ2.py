7e137b0832f12b02139da04e3bd701b6b0d11ae6
Vertigro <114555035+Vertigro@users.noreply.github.com>
2023-04-05 12:01:03 +0700

    Add files via upload

---
+++ 2полугодие/6ЕГЭ2.py
@@ -0,0 +1,12 @@
+from turtle import *
+for x in range(3):
+    forward(15*10)
+    right(120)
+       
+    
+pu()
+for x in range(0;10):
+    for y in range(0;10):
+        goto(x*10,y*10)
+        dot(5)
+done()
