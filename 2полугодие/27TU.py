e44162a6f6190298ed6c9066ba6eff173060feba
Vertigro <114555035+Vertigro@users.noreply.github.com>
2023-04-12 11:59:43 +0700

    Add files via upload

---
+++ 2полугодие/27TU.py
@@ -0,0 +1,13 @@
+with open('27_А.txt') as f:
+    n = [x for x in f]
+    print(n)
+    n.pop(0)
+    sp = []
+    for i in n:
+        sp.append(list(map(int(i.split())))
+    print(sp)
+
+for i in range(len(sp):
+               if sp[i][1]%36==0:
+                   sp[i][1]
+               
