97d6cf547f5c79011ccdf01cc675e494cc10b69d
Vertigro <114555035+Vertigro@users.noreply.github.com>
2023-05-04 11:57:29 +0700

    Add files via upload

--- 2полугодие/Алгоритмы.py
+++ 2полугодие/Алгоритмы.py
@@ -12,4 +12,38 @@ for x in range(len(a)):
         print(a[x],a[y])
 b = list(product('012',repeat=2))
 print(b) 
+def f(a): #Рекурсия
+    if a == 0: return 1
+    return f(a - 1)
+print(f(5))
+s = [1, 2, 7, 4, 5, 6] #Поиск максисума или убить дракона
+e = 0
+for i in range(len(s)):
+    si = s[i]
+    if si >= e:
+        e = si
+print(e)
+
+a = 123
+print(a+a)
+a = str(a)
+print(a+a)
+
+a = [x for x in range(101)] #5. Метод итераций или измерение на равноудаленных промежутках (доделать)
+b = [x for x in range(51)]
+b.reverse
+a = a + b
+print(a)
+for i in range(len(a)):
+    
+
+
+t1 = [2, 4, 5, 8]  #Если верен каждый или если где-то плохо, то все плохо (доделать)
+t = [2, 4, 6, 8]
+if all t[i]%2 == 0:
+   for i in range(len(t)):
+       print('список четный')
+
+
+        
 
